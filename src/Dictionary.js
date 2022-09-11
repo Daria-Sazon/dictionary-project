bb0d08528e8995a08066c069e4d8984b13997b92
Daria <daria.sazonenko12@gmail.com>
2022-09-11 20:40:51 +0300

    added word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import Results from "./Results";
 import './Dictionary.css';
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -24,5 +26,6 @@ export default function Dictionary() {
     <form onSubmit={search}>
       <input type="search" placeholder="Enter a word" onChange={handleKeywordChange} />
     </form>
+    <Results results={results} />
   </div>);
 }
