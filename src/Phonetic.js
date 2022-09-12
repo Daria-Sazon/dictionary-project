bed073a49dae39a6e9f024513860d95b0bd0b6fa
Daria <daria.sazonenko12@gmail.com>
2022-09-12 22:12:57 +0300

    added icon for listening

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Phonetic.css';
+import listen from './listen.png';
 
 export default function Phonetic(props) {
   return (
     <div className='Phonetic'>
       <a href={props.phonetic.audio} target='_blank' rel='noreferrer'>
-        Listen
-      </a>
+        <img src={listen} className='Phonetic-listen' img-fluid alt='listen' />
+      </a>      
       <span className='text'>{props.phonetic.text}</span>
     </div>
   );
