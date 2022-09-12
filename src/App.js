fa4454d0cd00b62dc7f51fca5f0f71bc33507f44
Daria <daria.sazonenko12@gmail.com>
2022-09-12 21:24:55 +0300

    added photos

--- src/App.js
+++ src/App.js
@@ -12,10 +12,12 @@ export default function App() {
           <img src={logo} className='App-logo' img-fluid alt='logo' />
         </header>
         <main>
-          <Dictionary defaultKeyword="sunset" />
+          <Dictionary defaultKeyword="smile" />
         </main>
         <footer className="App-footer">
-          <small>Coded by Daria Sazon</small>
+          <small>This project was coded by <a href='https://github.com/Daria-Sazon' target='_blank' rel='noreferrer'>Daria Sazon</a> and is <a
+            href='https://github.com/Daria-Sazon/dictionary-project' target='_blank' rel='noreferrer'> open-sourced on GitHub</a> and <a
+            href='https://elaborate-semifreddo-5455d6.netlify.app/' target='_blank' rel='noreferrer'>hosted on Netlify</a></small>
         </footer>
       </div>
     </div>
